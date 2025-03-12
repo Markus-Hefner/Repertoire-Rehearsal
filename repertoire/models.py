6b59cf8fe91aa15b92755f324fe6b88e8a4ca9dc
Markus-Hefner <m5zrlnlt@students.codeinstitute.net>
2025-03-12 11:36:56 +0100

    Add Song model and CSRF_TRUSTED_ORIGINS

--- repertoire/models.py
+++ repertoire/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=150, unique=True)
+    composer = models.CharField(max_length=150)
+    arranger = models.CharField(max_length=150, default="This is the original version")
+    info = models.TextField()
+    musician = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="songs"
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | by {self.composer} | arranged by {self.arranger}"
