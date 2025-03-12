d6bc59a86276965e70241b2dc8280a3fb45add83
Markus-Hefner <m5zrlnlt@students.codeinstitute.net>
2025-03-12 09:19:25 +0100

    Code ready for deployment

--- repertoire_rehearsal/settings.py
+++ repertoire_rehearsal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ao^#jn&=e(6fl=ot4dp4soqm%-b*@@y8be6h0y051*sddqk4d*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1',]
 
 
 # Application definition
