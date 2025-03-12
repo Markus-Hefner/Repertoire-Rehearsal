dab9336f79c4c629b79fd2ee3a6d080049667c30
Markus-Hefner <m5zrlnlt@students.codeinstitute.net>
2025-03-12 13:37:13 +0100

    Installed Summernote

--- repertoire/admin.py
+++ repertoire/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Song
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Song)
+class SongAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'composer', 'arranger')
+    search_fields = ['title']
+    list_filter = ('title', 'composer', 'arranger')
+    summernote_fields = ('info',)
+
 
 # Register your models here.
-admin.site.register(Song)
