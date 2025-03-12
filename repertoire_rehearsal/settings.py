cc62236d8fb7d8c4c4daa52c8c7e84508214eca2
Markus-Hefner <m5zrlnlt@students.codeinstitute.net>
2025-03-12 10:38:59 +0100

    Create superuser

--- repertoire_rehearsal/settings.py
+++ repertoire_rehearsal/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ao^#jn&=e(6fl=ot4dp4soqm%-b*@@y8be6h0y051*sddqk4d*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com',]
 
