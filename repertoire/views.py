c03ffb5237950f5a47ff3307d85cbf5abb38fb42
Markus-Hefner <m5zrlnlt@students.codeinstitute.net>
2025-03-13 23:41:08 +0100

    Add new view for editing songs

--- repertoire/views.py
+++ repertoire/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.contrib import messages
+from django.http import HttpResponseRedirect
 from .models import Song
 from .forms import AddSongForm
 
@@ -53,3 +54,23 @@ def song_rehearsal(request, id):
    )
 
 
+@login_required
+def song_edit(request, id):
+   """
+   View to edit songs
+   """
+   if request.method == "POST":
+
+      queryset = Song.objects.filter(user=request.user)
+      song = get_object_or_404(queryset, id=id)
+      comment = get_object_or_404(Comment, pk=comment_id)
+      add_song_form = AddSongForm(data=request.POST, instance=song)
+
+      if add_song_form.is_valid() and song.musician == request.user:
+         song = add_song_form.save(commit=False)
+         comment.save()
+         messages.add_message(request, messages.SUCCESS, 'Changes saved!')
+      else:
+         messages.add_message(request, messages.ERROR, 'Sorry, could not save changes.')
+
+   return HttpResponseRedirect(reverse('song_rehearsal', args=[id]))
