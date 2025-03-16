8e2d29361a73e499fab18cbf8ef9bd5a8a26ba51
Markus-Hefner <m5zrlnlt@students.codeinstitute.net>
2025-03-16 21:13:51 +0100

    Add musician to list_display in admin.py

--- repertoire/admin.py
+++ repertoire/admin.py
@@ -9,7 +9,7 @@ class SongAdmin(SummernoteModelAdmin):
     Lists fields for display in admin, fields for search,
     field filters and rich-text editor.
     """
-    list_display = ('title', 'id', 'composer', 'arranger')
+    list_display = ('title', 'id', 'composer', 'arranger', 'musician')
     search_fields = ['title', 'composer', 'arranger']
     list_filter = ('title', 'composer', 'arranger')
     summernote_fields = ('info',)
